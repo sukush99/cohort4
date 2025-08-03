62fc68c96c00017a3cf32f5b743dbdb00934b7b5
sukush99 <sukushkunwar99@gmail.com>
2025-08-03 21:41:28 +1000

    Add initial project structure with Python version, deployment scripts, and Kafka configuration

---
+++ deployments/kind/create_cluster.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Steps:
+
+# 1. Delete the cluster (if it exists, otherwise it will fail)
+echo "Deleting the cluster..."
+kind delete cluster --name rwml-34fa
+
+# 2. Delete the docker network (if it exists, otherwise it will fail)
+echo "Deleting the docker network..."
+docker network rm rwml-34fa-network
+
+# 3. Create the docker network
+echo "Creating the docker network..."
+docker network create --subnet 172.100.0.0/16 rwml-34fa-network
+
+# 4. Create the cluster
+echo "Creating the cluster..."
+KIND_EXPERIMENTAL_DOCKER_NETWORK=rwml-34fa-network kind create cluster --config ./kind-with-portmapping.yaml
---
+++ deployments/kind/install_kafka.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl create namespace kafka
+kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
+kubectl apply -f manifests/kafka-e11b.yaml
+
+
